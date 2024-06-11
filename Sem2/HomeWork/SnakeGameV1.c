0e727912d21a6c67f9b2e24c51bc269f8c638fc1
Oleg  Shuliak <nformals@gmail.com>
2024-06-12 00:49:19 +0300

    modify SnakeGameV1 -> add controls WSAD

--- Sem2/HomeWork/SnakeGameV1.c
+++ Sem2/HomeWork/SnakeGameV1.c
@@ -22,7 +22,7 @@ typedef struct snake_t {
 	int direction; 
 	size_t tsize; 
 	struct tail_t *tail; 
-	struct control_buttons controls; 
+	struct control_buttons *controls; 
 } snake_t;
 
 typedef struct tail_t { 
@@ -30,7 +30,9 @@ typedef struct tail_t {
 	int y; 
 } tail_t;
 
-struct control_buttons default_controls= {KEY_DOWN, KEY_UP, KEY_LEFT, KEY_RIGHT};
+
+struct control_buttons default_controls [3] = {{KEY_DOWN, KEY_UP, KEY_LEFT, KEY_RIGHT}, {'s', 'w', 'a', 'd'}, {'S', 'W', 'A', 'D'}};
+
 
 void initTail(struct tail_t t[], size_t size) { 
 	struct tail_t init_t={0,0}; 
@@ -51,7 +53,7 @@ void initSnake(snake_t *head, size_t size, int x, int y) {
 	initHead(head, x, y); 
 	head->tail = tail; // прикрепляем к голове хвост 
 	head->tsize = size+1; 
-	head->controls = default_controls; 
+	head->controls = default_controls;
 }
 
 void go(struct snake_t *head){ 
@@ -103,27 +105,31 @@ void goTail(struct snake_t *head) {
 }
 
 void changeDirection(snake_t* snake, const int32_t key) {    
-	if (key == snake->controls.down) {       
-		snake->direction = DOWN;    
-	} else if (key == snake->controls.up) {
-		snake->direction = UP;    
-	} else if (key == snake->controls.right) {       
-		snake->direction = RIGHT;    
-	} else if (key == snake->controls.left) {       
-		snake->direction = LEFT; 
+	for (int i = 0; i < 3; i++) {
+		if (key == snake->controls[i].down) {       
+			snake->direction = DOWN;    
+		} else if (key == snake->controls[i].up) {
+			snake->direction = UP;    
+		} else if (key == snake->controls[i].right) {       
+			snake->direction = RIGHT;    
+		} else if (key == snake->controls[i].left) {       
+			snake->direction = LEFT; 
+		}
 	}
 }
 
 int checkDirection(snake_t* snake, int32_t key) {
 	int checkDir = 1;
-	if (key == snake->controls.down && snake->direction == UP) {       
-		checkDir =0;    
-	} else if (key == snake->controls.up && snake->direction == DOWN) {
-		checkDir =0;    
-	} else if (key == snake->controls.right && snake->direction == LEFT) {       
-		checkDir =0;    
-	} else if (key == snake->controls.left && snake->direction == RIGHT) {       
-		checkDir =0; 
+	for (int i = 0; i < 3; i++) {
+		if (key == snake->controls[i].down && snake->direction == UP) {       
+			checkDir =0;    
+		} else if (key == snake->controls[i].up && snake->direction == DOWN) {
+			checkDir =0;    
+		} else if (key == snake->controls[i].right && snake->direction == LEFT) {       
+			checkDir =0;    
+		} else if (key == snake->controls[i].left && snake->direction == RIGHT) {       
+			checkDir =0; 
+		}
 	}
 	return checkDir;
 }
